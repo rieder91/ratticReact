332f99ff4f9b47a7090d75c49afc377ff264eae1
Thomas Rieder <thomas@rieder.io>
2016-03-13 23:51:24 +0100

    load full css up front

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  */
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './index.js',
@@ -16,7 +17,7 @@ module.exports = {
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react'},
-            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
             {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
@@ -32,6 +33,7 @@ module.exports = {
     },
 
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({minimize: true})
+        new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        new ExtractTextPlugin("styles.css")
     ]
 };
